82d3aff7e696bf3e5de30de982fcde4a95527653
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 01:25:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ function mapToNoChange(sourceArray) {
 }
 
 function mapToDouble(sourceArray) {
-  
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] = 2 * sourceArray[i];
+  }
+  return sourceArray;
 }
+
+for (let i = 0; i < sourceArray.length; i++) {
+  sourceArray[i] = - sourceArray[i];
+}
+return sourceArray;
