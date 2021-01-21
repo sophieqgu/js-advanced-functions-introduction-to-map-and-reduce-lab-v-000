8519e1a554f637605eac0b0bbcf9d2a77c22f014
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 01:23:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] = - sourceArray[i];
+  }
+}
+
+function mapToNoChange(sourceArray) {
+  
+}
