3a1b32ef480cdc53c664ca1bf944b1042c7f9ef4
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 01:35:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] = - sourceArray[i];
+  }
+  return sourceArray;
+}
+
+function mapToNoChange(sourceArray) {
+  return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] = 2 * sourceArray[i];
+  }
+  return sourceArray;
+}
+
+function mapToSquare(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    sourceArray[i] = sourceArray[i] ** 2;
+  }
+  return sourceArray;
+}
+
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    startingPoint += sourceArray[i];
+  }
+  return startingPoint;
+}
+
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) {
+      return true;
+    }
+  }
+  return false;
+}
