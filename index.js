18be1940f5efd1ab773b43f21a9fb30b6fc73ed6
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 01:24:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ function mapToNegativize(sourceArray) {
   for (let i = 0; i < sourceArray.length; i++) {
     sourceArray[i] = - sourceArray[i];
   }
+  return sourceArray;
 }
 
 function mapToNoChange(sourceArray) {
+  return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
   
 }
